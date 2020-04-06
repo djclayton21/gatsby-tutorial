a7534e25a4c783243233ca51cb7e9c24e1e8f41e
djclayton21 <clayton.daniel.j@gmail.com>
2020-04-06 12:08:50 -0600

    pages/component

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/Header.js"
+
+export default () => (
+  <div style={{ color: "green" }}>
+    <Header headerText="About Gatsby" />
+    <Header headerText="Its pretty fancy" />
+    <p>yup its pretty easy and fast</p>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import Header from "../components/Header"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: "blue", fontSize: "2rem" }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="still fancy" />
+    <p>this is pretty fancy</p>
+    <img
+      src="https://source.unsplash.com/random/400x200"
+      alt="random unsplash"
+    />
+  </div>
+)
